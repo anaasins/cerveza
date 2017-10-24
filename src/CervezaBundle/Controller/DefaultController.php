209a95f03fde099345cb338362d5ddd41676c163
Ana <asinsanna@gmail.com>
2017-10-24 21:12:31 +0200

    actividad 2

--- src/CervezaBundle/Controller/DefaultController.php
+++ src/CervezaBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace CervezaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use CervezaBundle\Entity\Cervezas;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CervezaBundle:Default:index.html.twig');
+      //devolver la clase para interactuar con la BBDD
+        $repository = $this->getDoctrine()->getRepository(Cervezas::class);
+      //sacar lo que queramos de la base de datos
+        $cervezas = $repository->findById(1);
+        return $this->render('CervezaBundle:Default:index.html.twig', array('cervezas'=>$cervezas));
     }
 }
