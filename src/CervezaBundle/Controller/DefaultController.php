1eb052f3a3eb7741985d2b716d47a79906ca1262
Ana Asins <asinsanna@gmail.com>
2017-11-03 11:44:10 +0100

    actividad 03

--- src/CervezaBundle/Controller/DefaultController.php
+++ src/CervezaBundle/Controller/DefaultController.php
@@ -16,12 +16,26 @@ class DefaultController extends Controller
         return $this->render('CervezaBundle:Default:index.html.twig', array('cervezas'=>$cervezas));
     }
 
-    public function insertarCervezaAction($nombre, $pais, $poblacion, $tipo)
+    public function insertarCervezaAction($nombre, $pais)
     {
-      //devolver la clase para interactuar con la BBDD
-        $repository = $this->getDoctrine()->getRepository(Cervezas::class);
-      //sacar lo que queramos de la base de datos
-        $cervezas = $repository->findById($id);
-        return $this->render('CervezaBundle:Default:index.html.twig', array('cervezas'=>$cervezas));
+      $em = $this->getDoctrine()->getManager();
+
+      $cerveza  = new Cervezas();
+      $cerveza->setNombre($nombre);
+      $cerveza->setPais($pais);
+      $cerveza->setPoblacion('poblacion');
+      $cerveza->setTipo('tipo');
+      $cerveza->setImportacion(true);
+      $cerveza->setTamano(20);
+      $cerveza->setFechaAlmacen(new \DateTime('10-10-2017'));
+      $cerveza->setCantidad(20);
+      $cerveza->setFoto('foto');
+
+      $em->persist($cerveza);
+
+      $em->flush();
+
+      return $this->redirectToRoute('index');
     }
+
 }
